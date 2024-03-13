d9deedd9e7e6c31612b323b847499d8b4ed708cb
Wei Wei <shazhouke@gmail.com>
2024-03-13 19:22:47 +0800

    Try to add a dummy api

--- service/index.ts
+++ service/index.ts
@@ -1,11 +1,28 @@
 import * as path from "node:path";
 import * as express from "express";
+import * as bodyParser from "body-parser";
 
 const app = express();
 
 const port = 3000;
 
-app.use("/", express.static(path.resolve(__dirname, '../dist')));
+app.get("/", (_, res) => {
+    res.redirect("/static/index.html");
+});
+
+app.use("/static", express.static(path.resolve(__dirname, '../dist')));
+
+const apiRoute = express.Router();
+
+apiRoute.use(bodyParser.json());
+
+apiRoute.post("/chat", (req, res) => {
+    res.json(req.body);
+    res.status(200);
+});
+
+app.use("/api", apiRoute);
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- src/main.tsx
+++ src/main.tsx
@@ -7,4 +7,25 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
+
+async function main() {
+  const res = await fetch("/api/chat", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: "foo",
+      password: "bar",
+    }),
+  });
+
+  const data = await res.json();
+
+  console.log(data);
+}
+
+main();
+
