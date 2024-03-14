04d1504fd42c30442814d2bb6f33d7f165ddaa1b
Scott Wei <leescott@microsoft.com>
2024-03-14 09:51:47 +0800

    Implement the simple chat ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,64 @@
-import { Stack, TextField } from "@fluentui/react";
+import { Stack, TextField, ITextField, PrimaryButton } from "@fluentui/react";
 // import "./App.css";
-import { MessageStream } from "./MessageStream";
+import { useCallback, useState, useRef } from "react";
+import { MessageStream, Message } from "./MessageStream";
 
-const content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui";
-const user = "OpenAI";
+// const content =
+//   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui";
+// const user = "OpenAI";
 
 function App() {
-	return (
-    <Stack styles={{
-			root: {
-				position: "fixed",
-				width: "100%",
-				height: "100%",
-				top: 0,
-			}
-		}}>
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [input, setInput] = useState("");
+  const [canSend, setCanSend] = useState(true);
+
+  const send = useCallback(() => {
+    if (canSend) {
+      console.log(input);
+      setMessages([...messages, { content: input, user: 'user' }]);
+      setInput("");
+    }
+  }, [canSend, input]);
+
+  return (
+    <Stack
+      styles={{
+        root: {
+          position: "fixed",
+          width: "100%",
+          height: "100%",
+          top: 0,
+        },
+      }}
+    >
       <Stack.Item
         styles={{
           root: {
             overflow: "scroll",
           },
         }}
+        grow={1}
       >
         <MessageStream
-          messages={new Array(10).fill({ content, user })}
+          messages={messages}
         ></MessageStream>
       </Stack.Item>
       <Stack.Item>
-        <TextField></TextField>
+        <Stack horizontal tokens={{ childrenGap: 5 }}>
+          <Stack.Item grow={1}>
+            <TextField
+              value={input}
+              onChange={useCallback((_: any, value: string = "") => setInput(value || ""), [])}
+            ></TextField>
+          </Stack.Item>
+          <Stack.Item>
+            <PrimaryButton
+              text="Send"
+              disabled={!canSend}
+              onClick={send}
+            ></PrimaryButton>
+          </Stack.Item>
+        </Stack>
       </Stack.Item>
     </Stack>
   );
