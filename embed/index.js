de37baaf6112e16ee9779b81f43a0d2c6c2ba74a
Scott Wei <leescott@microsoft.com>
2024-03-15 12:08:34 +0800

    Make the register window close automatically

--- embed/index.js
+++ embed/index.js
@@ -3,4 +3,5 @@ const bc = new BroadcastChannel("register");
 window.addEventListener("message", (event) => {
   const { origin, data } = event;
   bc.postMessage({ from: 'client', origin, data });
+  window.close();
 }, false);
